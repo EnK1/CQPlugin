0939bc5c4a4d779bbffe5f4439df42e60dbf6be3
Zero <xyyx0326@gmail.com>
2020-07-15 17:52:02 +0800

    修复一些bug

--- src/main/java/com/example/demo/plugin/StockPlugin.java
+++ src/main/java/com/example/demo/plugin/StockPlugin.java
@@ -17,7 +17,7 @@ public class StockPlugin extends CQPlugin {
         String msg1 = msg;
         long groupId = event.getGroupId();
 
-        if (msg.equals("大盘")){
+        if (msg.equals("大盘")) {
             String[] hqArr = StokHttp.getHqBySina("sh000001");
             String reply = "股票名称：" + hqArr[00] +
                     "\n今日开盘价：" + hqArr[01] +
@@ -26,29 +26,39 @@ public class StockPlugin extends CQPlugin {
                     "\n今日最高价：" + hqArr[04] +
                     "\n今日最低价：" + hqArr[05] +
                     "\n\n" + hqArr[30] + "\b-\b" + hqArr[31];
-            cq.sendGroupMsg(groupId,reply,false);
+            cq.sendGroupMsg(groupId, reply, false);
             return MESSAGE_BLOCK;
         }
-        if (msg.startsWith("sh")||msg.startsWith("sz")){
-            if (msg.length()==8){
-                String[] hqArr = StokHttp.getHqBySina(msg);
-                String reply = null;
+        if (msg.startsWith("sh") || msg.startsWith("sz")|| msg.startsWith("SZ")|| msg.startsWith("SH")) {
+            msg1 = msg1.substring(2);
+            if (msg.length() == 8) {
                 try {
-                    reply = Reply(hqArr,msg);
-                    cq.sendGroupMsg(groupId,reply,false);
-                } catch (Exception e) {
-                    cq.sendGroupMsg(groupId,"查询的股票代码不存在哦",false);
-                }
+                    Integer id = Integer.parseInt(msg1);
+                    if (id instanceof Integer == true) {
+                        try {
+                            String[] hqArr = StokHttp.getHqBySina(msg);
+                            String reply = null;
+                            reply = Reply(hqArr, msg);
+                            cq.sendGroupMsg(groupId, reply, false);
+                        } catch (Exception e) {
+                            cq.sendGroupMsg(groupId, "查询的股票代码不存在哦", false);
+                        }
 
-                return MESSAGE_BLOCK;
+                    }
+                } catch (NumberFormatException e) {
+                    cq.sendGroupMsg(groupId, "请不要输入奇怪的东西", false);
+                }
             }
+
+            return MESSAGE_BLOCK;
         }
+
         return MESSAGE_IGNORE;
     }
 
-    public static String Reply(String[] s,String msg){
+    public static String Reply(String[] s, String msg) {
         String picLink = "\n日K线： http://image.sinajs.cn/newchart/daily/n/" + msg + ".gif\n" +
-                "分时线： http://image.sinajs.cn/newchart/min/n/"+ msg + ".gif\n" +
+                "分时线： http://image.sinajs.cn/newchart/min/n/" + msg + ".gif\n" +
                 "周K线： http://image.sinajs.cn/newchart/weekly/n/" + msg + ".gif\n" +
                 "月K线： http://image.sinajs.cn/newchart/monthly/n/" + msg + ".gif\n";
         String reply = "股票名称：" + s[00] +
@@ -58,10 +68,9 @@ public class StockPlugin extends CQPlugin {
                 "\n今日最高价：" + s[04] +
                 "\n今日最低价：" + s[05] +
                 picLink +
-                s[30] + "\b-\b" + s[31];
+                s[30] + "-" + s[31];
         return reply;
     }
-
 
 
 }
